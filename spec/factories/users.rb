7cacf9ccd5066d5b3c62ec91d841f9ae10cd1285
natyudon <61405828+natyudon@users.noreply.github.com>
2020-09-07 12:15:30 +0900

    ユーザー管理機能、userモデル単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :user do
+    name {'kenshiro'}
+    email {'sukusuku@gmail.com'}
+    password {'syunkungood0101'}
+    first_name {'北斗'}
+    last_name {'健四郎'}
+    first_name_kana {'ホクト'}
+    last_name_kana {'ケンシロウ'}
+    birth_date {'1997-09-09'}
+  end
+end
