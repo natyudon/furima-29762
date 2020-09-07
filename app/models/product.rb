cba5da166b186fb0f244efa5d7276c2f16a122fc
natyudon <61405828+natyudon@users.noreply.github.com>
2020-09-07 19:01:13 +0900

    activeHash導入

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,24 @@
 class Product < ApplicationRecord
-  
+  belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :state, :delivery_fee, :area, :delivery_day
+
+  with_options presence: true do
+    validates :name
+    validates :content
+    validates :price
+    validates :category_id
+    validates :state_id
+    validates :delivery_fee_id
+    validates :area_id
+    validates :delivery_day_id
+  end
+  with_options numericality: { other_than: 1} do
+  validates :category_id
+  validates :state_id
+  validates :delivery_fee_id
+  validates :area_id
+  validates :delivery_day_id
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_072704) do
+ActiveRecord::Schema.define(version: 2020_09_07_092305) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
