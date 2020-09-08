a20f46b12d0872681aea1c07224c301347a83990
natyudon <61405828+natyudon@users.noreply.github.com>
2020-09-08 16:09:03 +0900

    productモデルvalidates設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,5 +5,17 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name,:content,:price,:image,:category_id,:state_id,:delivery_fee_id,:area_id,:delivery_day_id).merge(user_id: current_user.id)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,30 +1,18 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :state
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :delivery_fee
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :area
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :delivery_day
   belongs_to :user
   has_one_attached :image
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :state, :delivery_fee, :area, :delivery_day
 
-  with_options presence: true do
-    validates :name, length: { maximum: 40 }
-    validates :content, length: {maximum: 1000}
-    validates :price, numericality: { 
-      greater_than_or_equal_to: 300, 
-      less_than_or_equal_to: 9999999, 
-      message: "is out of setting range"
-    },
-      format: { with: /\A[0-9]+\z/, message: "は半角数字で入力してください"}
-    validates :category_id
-    validates :state_id
-    validates :delivery_fee_id
-    validates :area_id
-    validates :delivery_day_id
-    validates :image
-  end
-  with_options numericality: { other_than: 1} do
-  validates :category_id
-  validates :state_id
-  validates :delivery_fee_id
-  validates :area_id
-  validates :delivery_day_id
-  end
+  validates :name, :content, :category,:state, :delivery_fee,:area, :delivery_day, :image, presence: true
+  validates :category_id,:state_id,:delivery_fee_id,:area_id,:delivery_day_id, numericality: { other_than: 1, message: "Select"}
+  validates :price,presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 end
