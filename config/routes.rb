f7612c94ea2fcefd35400594f8a52b4f288d401f
natyudon <61405828+natyudon@users.noreply.github.com>
2020-09-09 12:35:15 +0900

    購入画面へのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
-  resources :products, only: [:new, :create,:show]
+  resources :products, only: [:new, :create,:show] do
+    resources :orders, only: :index
+  end
 end
