ed4784d99f4653cad4c0aeb30a5d51e12a36d3ac
natyudon <61405828+natyudon@users.noreply.github.com>
2020-09-09 16:05:58 +0900

    products/edit.html.erbの初期値設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
-  resources :products, only: [:new, :create,:show] do
+  resources :products, only: [:new, :create,:show,:edit] do
     resources :orders, only: :index
   end
 end
