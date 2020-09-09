252142d68e0c02a9575bce01422c4a5ab0c46986
natyudon <61405828+natyudon@users.noreply.github.com>
2020-09-09 17:34:37 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
-  resources :products, only: [:new, :create,:show,:edit,:update] do
+  resources :products do
     resources :orders, only: :index
   end
 end
