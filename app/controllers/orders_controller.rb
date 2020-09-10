f8ef2d2b058095bed512d492695ceb23021d511b
natyudon <61405828+natyudon@users.noreply.github.com>
2020-09-10 12:42:02 +0900

    createアクション実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,29 @@ class OrdersController < ApplicationController
     @product = Product.find(params[:product_id])
   end
   def create
-    
+    @product = Product.find(params[:product_id])
+    @order_address = OrderAddress.new(order_params [:postal_code],[:prefecture_id],[:municipalitie], [:address], [:building], [:phone_number])
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      return redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:token,:postal_code, :prefecture_id,:municipalitie, :address, :building, :phone_number).merge(product_id: @product.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @product.price,
+      card: order_params[:token],
+      currency:'jpy'
+    )
   end
 end
--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -1,7 +1,7 @@
 class OrderAddress
 
   include ActiveModel::Model
-  attr_accessor :user_id, :product_id, :postal_code, :prefecture_id, :municipalitie, :address, :building, :phone_number
+  attr_accessor :product_id, :postal_code, :prefecture_id, :municipalitie, :address, :building, :phone_number
 
   NUMBER_GOSIC = /\A[0-9０−９]+\z/.freeze
   POSTAL_VALIDATE = /\A[0-9]{3}-[0-9]{4}\z/.freeze
@@ -13,4 +13,9 @@ class OrderAddress
     validates :phone_number, format: {with: NUMBER_GOSIC}
   end
   validates :prefecture_id, numericality: { other_than: 1, message: 'Select'}
+
+  def save
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalitie: municipalitie, address: address, building: building, phone_number: phone_number)
+    Order.create(user_id: current_user.id, product_id: product_id)
+  end
 end
