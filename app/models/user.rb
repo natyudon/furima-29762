96f7646dc084b5c128b34d83e2d1688db52492cd
natyudon <61405828+natyudon@users.noreply.github.com>
2020-09-25 11:13:22 +0900

    sns機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,18 @@ class User < ApplicationRecord
   end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+
+    user = User.where(email: auth.info.email).first_or_initialize(
+     name: auth.info.name,
+       email: auth.info.email
+   )
+   if user.persisted?
+    sns.user = user
+    sns.save
+  end
+  { user: user, sns: sns }
+  end
 end
