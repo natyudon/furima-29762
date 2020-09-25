991b2d97d8b348db78a5429b05cecc0e979b1fcf
natyudon <61405828+natyudon@users.noreply.github.com>
2020-09-25 12:08:09 +0900

    sns_credential単体テストコード実装

--- spec/factories/sns_credentials.rb
+++ spec/factories/sns_credentials.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :sns_credential do
-    
+    provider {'google_oauth2'}
+    uid {'114931392076021536587'}
   end
 end
--- spec/models/sns_credential_spec.rb
+++ spec/models/sns_credential_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe SnsCredential, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'snsアカウント登録機能'do
+    before do
+      @sns_credential = FactoryBot.build(:sns_credential)
+    end
+
+    it '正しい値が存在していれば保存することができる'do
+      expect(@sns_credential).to be_valid
+    end
+
+    it 'providerの値が存在していなければ保存することができない' do
+      @sns_credential.provider = nil
+      @sns_credential.valid?
+      expect(@sns_credential.errors.full_messages).to include("Providerを入力してください")
+    end
+
+    it 'uidの値が存在していなければ保存することができない' do
+      @sns_credential.uid = nil
+      @sns_credential.valid?
+      expect(@sns_credential.errors.full_messages).to include("Uidを入力してください")
+    end
+  end
 end
