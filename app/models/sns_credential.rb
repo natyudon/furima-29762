ba9bb37c6374b8d7df280fb1e1037aa01b75ad57
natyudon <61405828+natyudon@users.noreply.github.com>
2020-09-25 11:44:08 +0900

    コメント追加

--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -1,3 +1,7 @@
 class SnsCredential < ApplicationRecord
   belongs_to :user, optional: true
+
+  validates :provider, presence: true
+  validates :uid, presence: true
+  #それぞれのカラムにnull: falseは指定していないけどテーブルに保存される時は両方の値が必要なためpresence: trueを指定
 end
