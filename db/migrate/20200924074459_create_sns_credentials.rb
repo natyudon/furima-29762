7ba3f939e6600a761c30a6560cc3c659e36a07a4
natyudon <61405828+natyudon@users.noreply.github.com>
2020-09-25 11:40:33 +0900

    コメント追加

--- db/migrate/20200924074459_create_sns_credentials.rb
+++ db/migrate/20200924074459_create_sns_credentials.rb
@@ -1,10 +1,13 @@
 class CreateSnsCredentials < ActiveRecord::Migration[6.0]
   def change
     create_table :sns_credentials do |t|
-      t.string :provider
+      t.string :provider     
       t.string :uid
       t.references :user, foreign_key: true
       t.timestamps
+  #:provider,:uidはnull: falseの記述はしないアソシエーションを組んでて
+  #sns認証を使わずにユーザー登録する人もいるためユーザー登録する際にnull: falseを
+  #指定しているとこのカラム入ってないとエラーになる可能性あるから
     end
   end
 end
